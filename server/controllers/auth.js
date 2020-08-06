76b1c454d45a5ff487dbfd231762fc5d7814ab06
Nguyen Hoang Thuc <t001899@cb.local>
2020-08-06 09:04:18 +0700

    update response data format.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -6,7 +6,9 @@ exports.sign_up = async (req, res) => {
 	let email_user = await UserService.getUserByEmail(req.body.email);
 	if (email_user) {
 		res.json({
+			message:'',
 			error: 'Email is already existed.',
+			data:{}
 		});
 	}
 	let hashed_password = await getHashedPassword(req.body.password);
@@ -20,15 +22,25 @@ exports.sign_up = async (req, res) => {
 		if (new_user) {
 			res.json({
 				message: 'Sign up success.',
+				error:'',
+				data:{
+					_id:new_user._id,
+					email:new_user.email,
+					display_name:new_user.display_name
+				}
 			});
 		} else {
 			res.json({
-				error: 'Oh no, something went wrong.',
+				message:'',
+				error: 'Sign up failed',
+				data:{}
 			});
 		}
 	} catch (error) {
 		res.status(500).json({
+			message:'',
 			error: 'Oh no, something went wrong.',
+			data:{}
 		});
 	}
 };
@@ -37,7 +49,9 @@ exports.sign_in = async (req, res) => {
 	let user = await UserService.getUserByEmail(req.body.email);
 	if (!user) {
 		res.json({
-			message: 'Email is not existed.',
+			message: '',
+			error: 'Email is not existed.',
+			data:{}
 		});
 	}
 	let password_check = await comparePassword(
@@ -46,18 +60,27 @@ exports.sign_in = async (req, res) => {
 	);
 	if (password_check) {
 		//Login Success
-		const token = jwt.sign({ mess: 'hello' }, process.env.PRIVATE_KEY, {
+		const user_info = {
+			_id: user._id,
+			email: user.email,
+			display_name: user.display_name,
+		};
+		const token = jwt.sign(user_info, process.env.PRIVATE_KEY, {
 			expiresIn: '1h',
 		});
 		res.json({
-			token,
-			user: {
-				_id: user._id,
-				email: user.email,
-				display_name: user.display_name,
+			message: 'Request succeed',
+			error:'',
+			data: {
+				token,
+				user: user_info,
 			},
 		});
 	} else {
-		res.json('Email and password is not matched.');
+		res.json({
+			message:'',
+			error:'Email and password are not matched.',
+			data:{}
+		});
 	}
 };
