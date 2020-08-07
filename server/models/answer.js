23e34587913051b133304b33f67a8ff4391acf20
hoanghuy-99 <52267873+hoanghuy-99@users.noreply.github.com>
2020-08-07 16:38:33 +0700

    add created_time and updated_time into question and answer

--- server/models/answer.js
+++ server/models/answer.js
@@ -1,21 +1,43 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var schema = new Schema({
-	content: {
-		type: String,
-		required: true,
+var schema = new Schema(
+	{
+		content: {
+			type: String,
+			required: true,
+		},
+		author: {
+			type: mongoose.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		question: {
+			type: mongoose.Types.ObjectId,
+			ref: 'Question',
+			required: true,
+		},
+		rating_detail: {
+			like_users: [
+				{
+					type: mongoose.Types.ObjectId,
+					ref: 'User',
+				},
+			],
+			dislike_users: [
+				{
+					type: mongoose.Types.ObjectId,
+					ref: 'User',
+				},
+			],
+		},
 	},
-	author: {
-		type: mongoose.Types.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-	question: {
-		type: mongoose.Types.ObjectId,
-		ref: 'Question',
-		required: true,
-	},
-});
+	{
+		timestamps: {
+			createdAt: 'created_time',
+			updatedAt: 'updated_time',
+		},
+	}
+);
 
 module.exports = mongoose.model('Answer', schema);
--- server/models/question.js
+++ server/models/question.js
@@ -1,49 +1,71 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-var schema = new Schema({
-	title: {
-		type: String,
-		required: true,
-	},
-	content: {
-		type: String,
-		required: true,
-	},
-	answers: [
-		{
+var schema = new Schema(
+	{
+		title: {
+			type: String,
+			required: true,
+		},
+		content: {
+			type: String,
+			required: true,
+		},
+		answers: [
+			{
+				type: mongoose.Types.ObjectId,
+				ref: 'Answer',
+			},
+		],
+		best_answer: {
 			type: mongoose.Types.ObjectId,
 			ref: 'Answer',
 		},
-	],
-	best_answer: {
-		type: mongoose.Types.ObjectId,
-		ref: 'Answer',
-	},
-	author: {
-		type: mongoose.Types.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-	followers: [
-		{
+		author: {
 			type: mongoose.Types.ObjectId,
 			ref: 'User',
 			required: true,
 		},
-	],
-	category: {
-		type: mongoose.Types.ObjectId,
-		ref: 'Category',
-		required: true,
-	},
-	tags: [
-		{
+		followers: [
+			{
+				type: mongoose.Types.ObjectId,
+				ref: 'User',
+				required: true,
+			},
+		],
+		category: {
 			type: mongoose.Types.ObjectId,
-			ref: 'Tag',
+			ref: 'Category',
 			required: true,
 		},
-	],
-});
+		tags: [
+			{
+				type: mongoose.Types.ObjectId,
+				ref: 'Tag',
+				required: true,
+			},
+		],
+		rating_detail: {
+			like_users: [
+				{
+					type: mongoose.Types.ObjectId,
+					ref: 'User',
+				},
+			],
+			dislike_users: [
+				{
+					type: mongoose.Types.ObjectId,
+					ref: 'User',
+				},
+			],
+		},
+	},
+	{
+		timestamps: {
+			createdAt: 'created_time',
+			updatedAt: 'updated_time',
+		},
+	}
+);
 
 module.exports = mongoose.model('Question', schema);
