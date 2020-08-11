798aa48d964fa25a3f3d2a18039c098a00a08657
hoanghuy-99 <52267873+hoanghuy-99@users.noreply.github.com>
2020-08-11 13:23:47 +0700

    add user service

---
+++ server/controllers/user.js
@@ -0,0 +1,5 @@
+require('dotenv').config();
+const UserService = require('../services/user');
+const response_format = require('../util/response_format');
+
+exports.
--- server/services/user.js
+++ server/services/user.js
@@ -13,10 +13,27 @@ class UserService {
 		return new_user;
 	}
 
-	static async getUserByEmail(email)
-	{
-		let user = await UserModel.findOne({email});
+	static async getUserByEmail(email) {
+		let user = await UserModel.findOne({ email });
 		return user;
+	}
+
+	static async getUserById(id) {
+		let user = await UserModel.findById(id);
+		return user;
+	}
+
+	static async update(id, { email, hashed_password, display_name }) {
+		try {
+			let user = await UserModel.findById(id);
+			user.email = email;
+			user.hashed_password = hashed_password;
+			user.display_name = display_name;
+			await user.save();
+		} catch {
+			return false;
+		}
+		return true;
 	}
 }
 
